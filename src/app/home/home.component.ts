b8ba3df9421cce182f788388c539b21beb2f907a
naman0058 <jnaman345@gmail.com>
2020-07-08 10:52:53 +0530

    a

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
@@ -16,6 +16,70 @@ export class HomeComponent implements OnInit {
     focus;
     focus1;
     constructor() { }
+ ngOnInit() {
+   $(document).ready(function () {
 
-    ngOnInit() {}
+
+
+     $('.send').click(function(){
+     if($('#name').val() == null || $('#name').val() == [] || $('#name').val() == "" || $('#name').val() == "null" ) alert("Enter Name...")
+     else  if($('#email').val() == null || $('#email').val() == [] || $('#email').val() == "" || $('#email').val() == "null" ) alert("Enter Email...")
+       else  if($('#number').val() == null || $('#number').val() == [] || $('#number').val() == "" || $('#number').val() == "null" ) alert("Enter Mobile Number...")
+        else  if($('#address').val() == null || $('#address').val() == [] || $('#address').val() == "" || $('#address').val() == "null" ) alert("Enter Address...")
+          else  if($('#comment').val() == null || $('#comment').val() == [] || $('#comment').val() == "" || $('#comment').val() == "null" ) alert("Enter Comment...")
+         else if(!$('#number').val().match('[0-9]{10}'))  {
+          alert("Please put 10 digit mobile number");
+          }
+          else{
+
+          var url ='http://sikkimfred.local.api/api/feedback'
+          let insertobj = {
+
+name : $('#name').val(),
+emailId : $('#email').val(),
+address : $('#address').val(),
+contactNumber : $('#number').val(),
+comments : $('#comment').val()
+
+
+          }
+
+console.log("InsertObj",insertobj);
+            fetch(url,{
+              method : 'POST',
+              body: JSON.stringify(insertobj),
+              headers : new Headers({
+               'Content-Type': 'application/json'
+              })
+              }).then(res=>res.json())
+              .catch(error => console.error("Error",error))
+              .then (result => {
+              console.log("response",result)
+              if(result.msg=="success") alert("Successfully Inserted");
+    else if(result.errors.EmailID) alert("Invalid Emailid");
+             else alert("An error occured..Please try again later");
+              
+              })
+
+
+
+
+
+          }
+        
+
+     })
+ 
+
+
+   
+
+
+    
+
+
+      });
+  
+  }
+
 }
